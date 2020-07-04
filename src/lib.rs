d6770f717ddd65d739a9b9aeecc70ca67ed01cc9
Josh McSavaney <mcsaucy@csh.rit.edu>
2020-07-04 16:19:14 -0400

    Add a disgusting sqlite-heavy unit test for playing around

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use bytes::Bytes;
 use ring::digest;
 use std::collections::HashMap;
+use std::rc::Rc;
 use std::vec::Vec;
 
 #[derive(Debug)]
@@ -236,6 +237,165 @@ mod tests {
                 Ok(have) => assert_eq!(want, have),
                 Err(err) => panic!("unexpected error {}", err),
             };
+            Ok(())
+        }
+    }
+    mod sqlite {
+        use super::*;
+
+        fn mkdb() -> rusqlite::Connection {
+            let conn = rusqlite::Connection::open_in_memory().unwrap();
+            conn.execute(
+                "CREATE TABLE data_leaves (
+                    digest  BLOB PRIMARY KEY,
+                    data BLOB NOT NULL
+                );",
+                rusqlite::NO_PARAMS,
+            )
+            .unwrap();
+            conn.execute(
+                "CREATE TABLE append_edges (
+                    digest BLOB NOT NULL,
+                    left BLOB NOT NULL,
+                    right BLOB NOT NULL,
+                    PRIMARY KEY(digest, left, right)
+                );
+                --CREATE TABLE patch_edges (
+                --    digest BLOB PRIMARY_KEY,
+                --    basis BLOB PRIMARY KEY,
+                --    patch BLOB PRIMARY KEY,
+                --);",
+                rusqlite::NO_PARAMS,
+            )
+            .unwrap();
+            return conn;
+        }
+
+        fn insert_dataleaf(conn: &rusqlite::Connection, leaf: &Leaf) -> rusqlite::Result<()> {
+            conn.execute(
+                "INSERT INTO data_leaves (digest, data) VALUES (?1, ?2);",
+                &[&leaf.digest, &leaf.data],
+            )?;
+            Ok(())
+        }
+        fn insert_appendedge(
+            conn: &rusqlite::Connection,
+            edge: &AppendEdge,
+        ) -> rusqlite::Result<()> {
+            conn.execute(
+                "INSERT INTO append_edges (digest, left, right) VALUES (?1, ?2, ?3);",
+                &[&edge.digest, &edge.left, &edge.right],
+            )?;
+            Ok(())
+        }
+
+        #[test]
+        fn sqlite() -> Result<(), rusqlite::Error> {
+            let conn = mkdb();
+            let a = raw(b"a");
+            let b = raw(b"b");
+            let c = raw(b"c");
+            let d = raw(b"d");
+
+            insert_dataleaf(&conn, &a)?;
+            insert_dataleaf(&conn, &b)?;
+            insert_dataleaf(&conn, &c)?;
+            insert_dataleaf(&conn, &d)?;
+
+            let bc = AppendEdge {
+                digest: sha256(b"bc"),
+                left: b.digest.clone(),
+                right: c.digest.clone(),
+            };
+
+            let bcd = AppendEdge {
+                digest: sha256(b"bcd"),
+                left: bc.digest.clone(),
+                right: d.digest.clone(),
+            };
+
+            let abcd = AppendEdge {
+                digest: sha256(b"abcd"),
+                left: a.digest.clone(),
+                right: bcd.digest.clone(),
+            };
+
+            insert_appendedge(&conn, &bc)?;
+            insert_appendedge(&conn, &bcd)?;
+            insert_appendedge(&conn, &abcd)?;
+
+            conn.execute(
+                "CREATE TEMPORARY TABLE edges AS
+                WITH RECURSIVE branches (digest, left, right) AS (
+                    SELECT digest, left, right
+                    FROM append_edges
+                    WHERE digest = ?1
+                        AND digest NOT IN (SELECT digest FROM data_leaves)
+
+                    UNION ALL
+
+                    SELECT child.digest, child.left, child.right
+                    FROM append_edges child
+                    JOIN branches parent
+                    ON child.digest IN (parent.left, parent.right)
+                )
+                SELECT * FROM branches;",
+                rusqlite::params![sha256(b"abcd")],
+            )?;
+
+            let mut edge_stmt = conn.prepare("SELECT * FROM edges")?;
+            let edges = edge_stmt.query_map(rusqlite::NO_PARAMS, |row| {
+                let edge = AppendEdge {
+                    digest: row.get(0)?,
+                    left: row.get(1)?,
+                    right: row.get(2)?,
+                };
+                Ok(edge)
+            })?;
+
+            let mut hippo = Hippo::new();
+            let mut digs = std::collections::HashSet::new();
+
+            for n in edges {
+                let edge = n?;
+
+                digs.insert(edge.digest.clone());
+                digs.insert(edge.left.clone());
+                digs.insert(edge.right.clone());
+
+                hippo.add(Node::Append(edge));
+            }
+            let values = Rc::new(
+                digs.iter()
+                    .filter(|d| hippo.check(d))
+                    .cloned()
+                    .map(rusqlite::types::Value::from)
+                    .collect::<Vec<rusqlite::types::Value>>(),
+            );
+
+            let mut find_leaves = conn.prepare(
+                "SELECT digest, data FROM data_leaves
+                WHERE
+                    digest IN (SELECT digest FROM edges)
+                    OR digest IN (SELECT left FROM edges)
+                    OR digest IN (SELECT right FROM edges);",
+            )?;
+            let leaves = find_leaves.query_map(rusqlite::NO_PARAMS, |row| {
+                let leaf = Leaf {
+                    digest: row.get(0)?,
+                    data: row.get(1)?,
+                };
+                Ok(leaf)
+            })?;
+
+            for n in leaves {
+                hippo.add(Node::Raw(n?));
+            }
+
+            let want = Bytes::from(b"abcd" as &[u8]);
+            let have = hippo.get(&sha256(b"abcd")).expect("failed to get abcd");
+            assert_eq!(want, have);
+
             Ok(())
         }
     }
